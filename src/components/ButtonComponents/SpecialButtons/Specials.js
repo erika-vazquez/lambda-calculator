0e206d800df72fabaa37997e2686ffa4adf03f06
erika-vazquez <ito2016@protonmail.ch>
2019-07-17 01:42:41 -0500

    Added imported data to state, returned button component for Specials

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,24 @@
 import React from "react";
+import {useState} from "react"; //+++ import for useState hook +++
 
 //import any components needed
+import SpecialButton from "./SpecialButton";
 
 //Import your array data to from the provided data file
+import {specials} from '../../../data';
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialState, setSpecialState] = useState(specials);
 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      <div className="specials">{specialState.map((specials)=>SpecialButton(specials))}</div>
     </div>
   );
 };
+//+++ export+++
+export default Specials;
